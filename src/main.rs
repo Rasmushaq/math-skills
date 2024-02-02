a3110523aef99bc038e214dd5ec6ea6660b2fe4e
Rasmushaq <Rasmus.a.j.haques@gmail.com>
2024-02-02 13:18:21 +0200

    asdsad

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn round_to_nearest<T: Float>(value: T) -> i64 {
     value.round().to_i64().unwrap_or_else(|| value.to_f64().unwrap_or(0.0) as i64)
 }
 
+
 fn main() {
     // Get the file path from the command-line arguments
     let args: Vec<String> = env::args().collect();
@@ -35,6 +36,7 @@ fn main() {
 
     // Calculate statistics
     let average: f64 = values.iter().sum::<i64>() as f64 / values.len() as f64;
+
     let variance = values
         .iter()
         .map(|&x| (x as f64 - average).powi(2))
@@ -46,7 +48,13 @@ fn main() {
     println!("Average: {}", round_to_nearest(average));
     let mut sorted_values = values.clone();
     sorted_values.sort();
+    if sorted_values[values.len() / 2] / sorted_values[values.len() / 2 - 1] == 1{
     println!("Median: {}", sorted_values[values.len() / 2]);
+    } else {
+        let median1 =  sorted_values[values.len()/2-1];
+        let median2:  i64=sorted_values[values.len()/2];
+        println!("Median: {:.1}", (median1 + median2)/2);
+    }
     println!("Variance: {}", round_to_nearest(variance));
     println!("Standard Deviation: {}", std_deviation);
 }
